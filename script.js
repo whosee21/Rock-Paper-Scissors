5b034198d292ab08532e7d50bc61a2a26be3c031
whosee21 <kidacoe21@yahoo.com>
2025-01-10 19:04:47 +0800

    Removed pseudocode comments

--- script.js
+++ script.js
@@ -1,30 +1,3 @@
-// Pseudocode
-
-//  create a function for computer choice
-
-//     make a variable that handle the choices "rock" "paper" and "scissors"
-//     generate random numbers that returns 1 of 3 choices
-//     use conditional statements to make a choice
-//     store that choice in the variable
-//     return the variable
-
-//  create new function for user choice
- 
-//     make a prompt so that the user can choose between rock, paper, or scissors
-//     create a variable to store the user chose
-//     return the variable
- 
-//  make 2 variables to track players scores with intial score of 0
-
-//  make a function to play a round
- 
-//     make the choices case-insensitive so that it will be user-friendly
-//     If else statements to determine the round winner
-
-// make the game to have 5 rounds and declare the winner in the end 
-//  can use for loop or while loop
-
-
 console.log("Let's play Rock, Paper, and Scissors!");
 
 function getComputerChoice() {
